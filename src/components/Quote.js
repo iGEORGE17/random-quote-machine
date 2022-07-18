f7d801b6e00dd8f8d960184a9e3e55ebb01fe89d
iGEORGE <www.igeorge.www@gmail.com>
2022-07-18 06:56:44 +0100

    mades changes to share on tumblr

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -49,10 +49,10 @@ class Quote extends Component {
             }
           <div id="actionButtons">
             <div id="share-icons">
-                <a style={{backgroundColor:this.state.accentColor}} href={encodeURI(`http://www.twitter.com/intent/tweet?text=${this.state.quote} - ${this.state.author}`)} target="_blank" rel="noreferrer"  id='tweet-quote'><i className='fa-brands fa-twitter'></i></a>
+                <a style={{backgroundColor:this.state.accentColor}} href={encodeURI(`http://www.twitter.com/intent/tweet?text=${this.state.quote} - ${this.state.author}`)} target="_blank" rel="noreferrer" title="Share on twitter"  id='tweet-quote'><i className='fa-brands fa-twitter'></i></a>
                 <a style={{backgroundColor:this.state.accentColor}} 
                 href={encodeURI(`https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=${this.state.author}&content=${this.state.quote}&canonicalUrl=https://www.tumblr.com/buttons&shareSource=tumblr_share_button`)} 
-                target="_blank" rel="noreferrer" id='tumblr-quote'><i className='fa-brands fa-tumblr'></i></a>
+                target="_blank" rel="noreferrer" title="Share on tumblr" id='tumblr-quote'><i className='fa-brands fa-tumblr'></i></a>
             </div>
             <button id='new-quote' onClick={this.generateNewQuote} style={{backgroundColor:this.state.accentColor}}>New quote</button>
           </div>
